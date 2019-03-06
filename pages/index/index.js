68f38c9cf751666ccbfc02c34295a943f5d8bfc5
wujunjie <917436062@qq.com>
2019-03-06 11:41:16 +0800

    首页，搜索

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,49 +3,65 @@
 const app = getApp()
 
 Page({
-    data: {
-        imgUrls: [
-            'http://imagecache.pzlife.vip/o_1ct9qbhu31idf197n1fgoej1k737.jpg?imageMogr2/thumbnail/720x',
-            'http://imagecache.pzlife.vip/o_1ct9qbhu31idf197n1fgoej1k737.jpg?imageMogr2/thumbnail/720x',
-            'http://imagecache.pzlife.vip/o_1ct9qbhu31idf197n1fgoej1k737.jpg?imageMogr2/thumbnail/720x'
-        ],
-        motto: 'Hello World',
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        imgHost: ''
-    },
-    //事件处理函数
-    bindViewTap: function() {
-        wx.navigateTo({
-            url: '../logs/logs'
-        })
-    },
-    /**
-     * 下拉刷新
-     */
-    onPullDownRefresh: function() {
-        wx.showNavigationBarLoading() //在标题栏中显示加载
-            //模拟加载
-        setTimeout(function() {
-            wx.hideNavigationBarLoading() //完成停止加载
-            wx.stopPullDownRefresh() //停止下拉刷新
-        }, 1500);
-    },
-    onLoad: function() {
-        this.setData({
-            imgHost: app.globalData.host.imgHost
-        })
-    },
-    bindRegionChange: function(e) {
-        console.log(e)
-    },
-    getUserInfo: function(e) {
-        console.log(e)
-        app.globalData.userInfo = e.detail.userInfo
-        this.setData({
-            userInfo: e.detail.userInfo,
-            hasUserInfo: true
-        })
-    }
+	data: {
+		imgUrls: [
+			app.globalData.host.imgHost + "banner.png",
+			app.globalData.host.imgHost + "banner.png",
+			app.globalData.host.imgHost + "banner.png",
+		],
+		motto: 'Hello World',
+		userInfo: {},
+		hasUserInfo: false,
+		canIUse: wx.canIUse('button.open-type.getUserInfo'),
+		imgHost: '',
+		num:1,
+		pos_scroll:0
+	},
+	//事件处理函数
+	bindViewTap: function() {
+		wx.navigateTo({
+			url: '../logs/logs'
+		})
+	},
+	swiper:function(e){
+		this.setData({
+			num:parseInt(e.detail.current)+1
+		})
+	},
+	movescroll:function(e){
+		console.log(e)
+		if(e.detail.scrollLeft >=500){
+			e.detail.scrollLeft = 500
+		}
+		this.setData({
+			pos_scroll:parseInt(e.detail.scrollLeft)
+		})
+	},
+	/**
+	 * 下拉刷新
+	 */
+	onPullDownRefresh: function() {
+		wx.showNavigationBarLoading() //在标题栏中显示加载
+		//模拟加载
+		setTimeout(function() {
+			wx.hideNavigationBarLoading() //完成停止加载
+			wx.stopPullDownRefresh() //停止下拉刷新
+		}, 1500);
+	},
+	onLoad: function() {
+		this.setData({
+			imgHost: app.globalData.host.imgHost
+		})
+	},
+	bindRegionChange: function(e) {
+		console.log(e)
+	},
+	getUserInfo: function(e) {
+		console.log(e)
+		app.globalData.userInfo = e.detail.userInfo
+		this.setData({
+			userInfo: e.detail.userInfo,
+			hasUserInfo: true
+		})
+	}
 })
