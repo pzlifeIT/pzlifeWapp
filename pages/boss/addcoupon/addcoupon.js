175517b1d102ee7c1a69bd497559b7b47ff47300
wujunjie <917436062@qq.com>
2019-03-12 17:13:47 +0800

    修改

--- pages/boss/addcoupon/addcoupon.js
+++ pages/boss/addcoupon/addcoupon.js
@@ -6,9 +6,49 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
-		imgHost: ''
+		imgHost: '',
+		startDate:"2018.09.15",
+		endDate:"2019.01.03",
+		title:"",
+		subtitle:"",
+		number:0,
+		full:0,
+		subtract:0
 	},
-
+	inputwacth:function(e){
+		console.log(e)
+		let item = e.currentTarget.dataset.model,
+			model = e.detail.value
+		this.setData({
+			[item]:model
+		})
+	},
+    selectStartDate:function(e){
+		console.log(e)
+		let endDate = this.data.endDate
+		if (e.detail.value > endDate){
+			app.toast({
+				title:"开始时间不能大于结束时间"
+			})
+			return
+		}
+		this.setData({
+			startDate:e.detail.value
+		})
+	},
+    selectEndDate:function(e){
+        console.log(e)
+		let start = this.data.startDate
+		if (e.detail.value < start){
+			app.toast({
+				title:"结束时间不能小于开始时间"
+			})
+			return
+		}
+        this.setData({
+            endDate:e.detail.value
+        })
+    },
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
