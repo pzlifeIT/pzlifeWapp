7a270b826f04083a1a52e757532830a390a579ae
zhups <1194286917@qq.com>
2019-04-09 11:01:20 +0800

    订单详情商品跳转

--- pages/order/logisticslist/logisticslist.js
+++ pages/order/logisticslist/logisticslist.js
@@ -15,6 +15,9 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
+        wx.setNavigationBarTitle({
+            title: '订单物流'
+        })
         this.setData({
             order_no: options.orderno
         })
