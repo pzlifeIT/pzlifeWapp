aba06dec48d88af32b42845eddcdf6bc2a022ce5
zhups <1194286917@qq.com>
2019-04-11 19:17:15 +0800

    更新

--- pages/boss/withdraw/submitStatus/submitStatus.js
+++ pages/boss/withdraw/submitStatus/submitStatus.js
@@ -7,30 +7,30 @@ Page({
      */
     data: {
         imgHost: "",
-        status:0
+        status: 0
     },
-    cha:function(){
-      wx.navigateTo({
-          url:"/pages/boss/withdraw/record/record?div=1"
-      })
-    },
-    goon:function(){
-      wx.navigateTo({
-          url:"/pages/boss/withdraw/withdraw"
-      })
-    },
-    goBoss:function(){
+    cha: function() {
         wx.navigateTo({
-            url:"/pages/boss/boss"
+            url: "/pages/boss/withdraw/record/record?div=1"
+        })
+    },
+    goon: function() {
+        wx.navigateTo({
+            url: "/pages/boss/withdraw/withdraw"
+        })
+    },
+    goBoss: function() {
+        wx.navigateTo({
+            url: "/pages/my/my"
         })
     },
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function(options) {
         this.setData({
             imgHost: app.globalData.host.imgHost,
-            status:options.status
+            status: options.status
         })
 
     },
@@ -38,49 +38,49 @@ Page({
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
+    onReady: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
+    onShow: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {
+    onHide: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {
+    onUnload: function() {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
+    onPullDownRefresh: function() {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
+    onReachBottom: function() {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage: function() {
 
     }
 })
