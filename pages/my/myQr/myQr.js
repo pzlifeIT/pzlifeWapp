4d126909a017173850a9ef7924935316382aff94
wujunjie <917436062@qq.com>
2019-07-02 15:53:18 +0800

    保存二维码，钻石会员导航栏入口

--- pages/my/myQr/myQr.js
+++ pages/my/myQr/myQr.js
@@ -12,16 +12,16 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
+    onLoad: function (options) {
         this.getUserQrcode()
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function() {
+    onReady: function () {
 
     },
-    getUserQrcode: function() {
+    getUserQrcode: function () {
         let that = this,
             pid = app.globalData.userInfo.uid
         app.wxrequest({
@@ -44,87 +44,94 @@ Page({
             }
         })
     },
-    saveImg: function (e) {
-        console.log(e)
+    save: function (e) {
         let img = e.currentTarget.dataset.img;
-        wx.authorize({
-            scope: "scope.writePhotosAlbum",
-            success(res) {
-                console.log(res)
-                console.log("授权了")
-                wx.downloadFile({
-                    url: img,
-                    success(res) {
-                        console.log("下载成功")
-                        wx.saveImageToPhotosAlbum({
-                            filePath:res.tempFilePath,
-                            success(res){
-                                console.log("保存成功")
-                                app.toast({
-                                    title:"保存成功，请进入相册查看"
-                                })
-                            }
-                        })
-                    }
-                })
-            },
-            fail(res) {
-                console.log("没授权")
-                app.modal({
-                    content: "您未授权保存到相册，请点击确定打开权限",
-                    success(res) {
-                        wx.openSetting({
-                            success(res){
-                                app.toast({
-                                    title: "请再次长按图片保存"
-                                })
-                            }
-                        })
-                    }
-                })
-
-            }
+        wx.previewImage({
+            current: img,
+            urls: [img]
         })
     },
+    // saveImg: function (e) {
+    //     console.log(e)
+    //     let img = e.currentTarget.dataset.img;
+    //     wx.authorize({
+    //         scope: "scope.writePhotosAlbum",
+    //         success(res) {
+    //             console.log(res)
+    //             console.log("授权了")
+    //             wx.downloadFile({
+    //                 url: img,
+    //                 success(res) {
+    //                     console.log("下载成功")
+    //                     wx.saveImageToPhotosAlbum({
+    //                         filePath:res.tempFilePath,
+    //                         success(res){
+    //                             console.log("保存成功")
+    //                             app.toast({
+    //                                 title:"保存成功，请进入相册查看"
+    //                             })
+    //                         }
+    //                     })
+    //                 }
+    //             })
+    //         },
+    //         fail(res) {
+    //             console.log(res)
+    //             app.modal({
+    //                 content: "您未授权保存到相册，请点击确定打开权限",
+    //                 success(res) {
+    //                     wx.openSetting({
+    //                         success(res){
+    //                             app.toast({
+    //                                 title: "请再次长按图片保存"
+    //                             })
+    //                         }
+    //                     })
+    //                 }
+    //             })
+    //
+    //         }
+    //     })
+    // },
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function() {
+    onShow: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function() {
+    onHide: function () {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function() {
+    onUnload: function () {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function() {
+    onReachBottom: function () {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function() {
+    onShareAppMessage: function () {
 
     }
 })
